f62df7ec14aeff4b109ea5b746052060c5bdddbb
jsolaura <jke@peopulley.com>
2024-01-11 14:47:23 +0900

    push 형태 수정

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,17 +1,20 @@
 "use server";
 
-import { CheckoutOrderParams, CreateOrderParams } from '@/types';
-import { Bootpay } from '@bootpay/client-js'
-import { redirect } from 'next/navigation';
-import { connectToDatabase } from '../database';
+import { CheckoutOrderParams, CreateOrderParams, GetOrdersByEventParams, GetOrdersByUserParams } from "@/types"
+import { redirect, useRouter } from 'next/navigation';
 import { handleError } from '../utils';
+import { connectToDatabase } from '../database';
 import Order from '../database/models/order.model';
+import Event from '../database/models/event.model';
+import User from '../database/models/user.model';
+import {ObjectId} from 'mongodb';
 
 export const checkoutOrder = async (isSuccessed?: boolean) => {
+    const Router = useRouter();
     if(isSuccessed) {
-        redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/profile`);
+        Router.push(`/profile`);
     } else {
-        redirect(`${process.env.NEXT_PUBLIC_SERVER_URL}/`);
+        Router.push(`/`);
     }
 }
 
@@ -28,5 +31,35 @@ export const createOrder = async (order: CreateOrderParams) => {
     return JSON.parse(JSON.stringify(newOrder));
     } catch (error) {
         handleError(error);
+    }
+}
+
+export async function getOrdersByUser({ userId, limit = 3, page }: GetOrdersByUserParams) {
+    try {
+        await connectToDatabase()
+
+        const skipAmount = (Number(page) - 1) * limit
+        const conditions = { buyer: userId }
+
+        const orders = await Order.distinct('event._id')
+        .find(conditions)
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(limit)
+        .populate({
+            path: 'event',
+            model: Event,
+            populate: {
+            path: 'organizer',
+            model: User,
+            select: '_id firstName lastName',
+            },
+        })
+
+        const ordersCount = await Order.distinct('event._id').countDocuments(conditions)
+
+        return { data: JSON.parse(JSON.stringify(orders)), totalPages: Math.ceil(ordersCount / limit) }
+    } catch (error) {
+        handleError(error)
     }
 }
